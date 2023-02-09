9c8dc72860d88753ba5fe52dd249997eac4ba757
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-02-09 02:45:54 +0000

    Add GROUP BY and HAVING query

---
+++ 2_queries_joins/3_submissions_by_student.sql
@@ -0,0 +1,7 @@
+SELECT students.name AS student, COUNT(assignment_submissions.*) AS total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING COUNT(assignment_submissions.*) < 100
+ORDER BY students.name;
